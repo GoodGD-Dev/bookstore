869352661650de63fe480c96231c706b8e84b2e8
GoodGD <dionatha.work@gmail.com>
2025-02-10 16:40:59 -0300

    paginação add

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
@@ -30,6 +30,7 @@ router.register(r'product', ProductViewSet, basename='product')
 
 # Definição das URLs principais
 urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
     re_path(r"^bookstore/(?P<version>(v1|v2))/", include(router.urls)),  # Inclui todas as rotas do DRF
 ]
