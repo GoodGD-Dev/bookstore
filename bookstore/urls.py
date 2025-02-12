29de7e942cbc7da98d4a3d2ed289f8f64024383e
GoodGD <dionatha.work@gmail.com>
2025-02-12 16:19:14 -0300

    feat: add auth-token

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for bookstore project.
+"""bookstore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,21 +15,13 @@ Including another URLconf
 """
 import debug_toolbar
 from django.contrib import admin
-from django.urls import path, include, re_path
-from rest_framework import routers
+from django.urls import path, re_path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
-# Importação dos viewsets
-from order.viewsets import OrderViewSet
-from product.viewsets import ProductViewSet
-
-# Criando roteadores para os endpoints da API
-router = routers.SimpleRouter()
-router.register(r'order', OrderViewSet, basename='order')
-router.register(r'product', ProductViewSet, basename='product')
-
-# Definição das URLs principais
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
-    path("admin/", admin.site.urls),
-    re_path(r"^bookstore/(?P<version>(v1|v2))/", include(router.urls)),  # Inclui todas as rotas do DRF
+    path('admin/', admin.site.urls),
+    re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
+    re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
